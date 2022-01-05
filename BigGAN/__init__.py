b8036d1120a86a0755dd954c667d041280f65bb2
JulianProgress <imjulian@korea.ac.kr>
2022-01-05 10:01:54 +0900

    update project

---
+++ BigGAN/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Created   : 2021/09/09 8:07 오후
+# @Author    : Junhyung Kwon
+# @Site      : 
+# @File      : __init__.py.py
+# @Software  : PyCharm
+from .biggan import Generator, Discriminator, G_D
